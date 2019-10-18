83755517d68348fc9ec0af03627cffa198c589c5
drpk <pnadig1@gmail.com>
2019-10-18 00:03:45 -0400

    Add files via upload

---
+++ src/me/pramukh/euler/Main.java
@@ -0,0 +1,22 @@
+package me.pramukh.euler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+       int x=0;
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            if(!list.contains(i)) {
+                if (i % 3 == 0 || i % 5 == 0) {
+                    list.add(i);
+                }
+            }
+        }
+        for (Integer integer : list) {
+            x = x + integer;
+        }
+        System.out.println(x);
+    }
+}
